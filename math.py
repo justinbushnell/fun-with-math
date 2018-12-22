09f32661f4d100d80fa5a4750d16466082566914
Justin Bushnell <justinbushnell@Justins-MacBook-Air.local>
2018-12-21 22:39:23 -0500

    Add it back

---
+++ math.py
@@ -0,0 +1,20 @@
+def sum(x, y):
+    z=0
+    z=x+y
+    return z
+
+def subtract(x, y):
+    z=0
+    z=x-y
+    return z
+
+def multiply(x, y):
+    z=0
+    z=x*y
+    return z
+
+def division(x, y):
+    return x / y
+
+def square(n):
+    return n*n
